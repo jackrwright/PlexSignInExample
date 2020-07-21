d7bb5dcf317f1d291e0605463d5e33df93337e90
Jack Wright <jackrwright1@gmail.com>
2020-07-21 14:22:36 -0400

    Initial Commit

---
+++ PlexSignInExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlexSignInExample
+//
+//  Created by Jack Wright on 7/21/20.
+//  Copyright © 2020 Jack Wright. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
